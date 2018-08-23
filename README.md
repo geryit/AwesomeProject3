afdb2ebd0ba9748d3d9cf10bc0114ec699f74728
Goksel Eryigit <geryit@gmail.com>
2018-08-23 13:16:38 -0700

    read

---
+++ README.md
@@ -0,0 +1,10 @@
+# AwesomeProject
+No bundle URL present : `rm -rf ios/build/; kill $(lsof -t -i:8081);`
+
+```shell
+yarn ios
+yarn web
+```
+
+
+
--- index.web.js
+++ index.web.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './web/src/index.css';
 import App from './src/App';
-import registerServiceWorker from './web/registerServiceWorker';
+// import registerServiceWorker from './public/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
